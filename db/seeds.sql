b3b1a77bc4cf559715935579b0e1a4239064c981
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-01-25 16:54:07 -0600

    setting up database

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheese", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mushroom", false);
