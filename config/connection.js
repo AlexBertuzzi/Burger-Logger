5ca20cc80f0f2ce41ae9cf62f8fd6a406e15a358
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-01-26 20:58:04 -0600

    enabling heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSBD_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Amb-19376482",
+    database: "burgers_db"
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
